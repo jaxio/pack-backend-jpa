9608b00fcaa94b4790c0a1b52ccb1e9574bc88a4
Nicolas Romanetti <nromanetti@macbook-pro-de-nicolas.home>
2015-11-03 10:41:16 +0100

    fix compilation

--- src/main/resources/celerio/pack-backend-jpa/src/test/java/repository/support/SearchUsingManyToManyIT.p.vm.java
+++ src/main/resources/celerio/pack-backend-jpa/src/test/java/repository/support/SearchUsingManyToManyIT.p.vm.java
@@ -18,6 +18,7 @@ $output.javaTest($RepositorySupport, "SearchUsingManyToManyIT")
 $output.requireStatic("com.google.common.collect.Lists.newArrayList")##
 $output.requireStatic("org.fest.assertions.Assertions.assertThat")##
 $output.require("javax.inject.Inject")##
+$output.require("com.jaxio.jpa.querybyexample.SearchParameters")##
 $output.require("org.junit.Test")##
 $output.require("org.junit.runner.RunWith")##
 $output.require("org.springframework.test.context.ContextConfiguration")##
